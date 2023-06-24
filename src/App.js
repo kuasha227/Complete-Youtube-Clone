56c684b45f2eada6cea944569693a834b9667a7a
kuasha227 <kuasha227@gmail.com>
2023-06-24 22:42:17 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Box } from "@mui/material";
-import Navbar from "./components/Navbar";
-import Feed from "./components/Feed";
-import ChannelDetail from "./components/ChannelDetail";
-import VideoDetail from "./components/VideoDetail";
-import SearchFeed from "./components/SearchFeed";
+import { Box } from '@mui/material';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Box sx={{ backgroundColor: "#000" }}>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Feed />} />
-          <Route path="/video/:id" element={<VideoDetail />} />
-          <Route path="/channel/:id" element={<ChannelDetail />} />
-          <Route path="/search/:searchTerm" element={<SearchFeed />} />
-        </Routes>
-      </Box>
-    </BrowserRouter>
-  );
-}
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -1,24 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
   params: {
     maxResults: 50,
   },
   headers: {
-    channelId: "UCBVjMGOIkavEAhyqpxJ73Dw",
-    part: "snippet,id",
-    order: "date",
-    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
-    "Content-Type": "application/json",
-    Accept: "application/json",
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 };
 
-export const fetchFromApi = async (url) => {
-  const { data } = await axios.request(`${BASE_URL}/${url}`, options);
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
   return data;
 };
